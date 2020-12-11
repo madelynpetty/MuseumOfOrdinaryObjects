70839dd913b3df7f4626eedc860f384e8bb5cdae
Maddie Petty <madelyn.johnson@gmail.com>
2020-12-11 03:11:53 +0000

    final commit

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -15,6 +15,7 @@
   </div>
   <div class="footer">
     <router-link to="/admin">Admin</router-link>
+    <p><a href="https://github.com/BYU-CS-260-Winter-2020/lab-4-museum-of-ordinary-objects-madelynjohnsonnn.git">GITHUB LINK</a></p>
   </div>
 </div>
 </template>
